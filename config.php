fc4cfe245af739c4b9a46466a4836be73ac73996
ducit1995 <ducnguyen12101995@gmail.com>
2018-07-17 12:33:27 +0700

    update config.php
    update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
 
-const LYRICMERCH_USER_ID  = 577;
-const LYRICMERCH_API_KEY  = 'd8a1dedda00d8b057d2adc09fcef26d7';
-const URL_API  = 'http://lyricmerch.xxx/wp-json/mobilefolk/v1';
-const REQUEST_URI  = '/wp-json/mobilefolk/v1';
+const LYRICMERCH_USER_ID  = 1;
+const LYRICMERCH_API_KEY  = 'token';
+const URL_API  = 'https://lyricmerch.com/api/v1';
+const REQUEST_URI  = '/api/v1';
